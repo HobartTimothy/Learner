81c8a7d3ddc2fe146771fdceb368b43e5f96f62d
RobertHU <18256077691@163.com>
2026-07-21 15:08:05 +0800

    chore(*): 添加项目

---
+++ PyTutorial/basal/print_output/output.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2026/7/21 12:27
+# @Author  : Robert.HU
+# @Email   : 18256077691@163.com
+# @File    : output.py
+# @Software: PyCharm
+# 
+# @License : Robert.HU (C)Copyright 2018-2021
+
+# print()内建函数用于打印输出，默认打印到标准输出 sys.stdout
+# print(value, ..., sep=' ', end='\n', file=sys.stdout, flush=False)
+
+# 打印输出和格式化
+
+if __name__ == '__main__':
+    print("Hello %s. Year: %d, Month: %d" % ("World", 2026, 7))
--- PyTutorial/basal/var_datatype/data_type.py
+++ PyTutorial/basal/var_datatype/data_type.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
-# @Time    : 2026/7/17 20:39
+# @Time    : 2026/7/18 19:27
 # @Author  : Robert.HU
 # @Email   : 18256077691@163.com
-# @File    : main.py
+# @File    : data_type.py
 # @Software: PyCharm
 # 
 # @License : Robert.HU (C)Copyright 2018-2021
 
+def str_data_type():
+    # 转义字符测试
+    print("I\'m \"Ok\". Are You\' \"Ok?\" ")
+
+
 if __name__ == '__main__':
-    print("Hello World")
+    str_data_type()
